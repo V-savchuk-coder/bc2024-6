f7887587b6f83c0f2fa827a8ca2f3f2669ecb0f5
Vladuslav bc2024-6 <vladuslavsava@gmail.com>
2024-11-24 23:27:12 +0200

    add documantation

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const { Command } = require('commander');
 const fs = require('fs');
+const fsSync = require('fs');
+
 const path = require('path');
 const bodyParser = require('body-parser');
 const multer = require('multer');
-
+const http = require('http');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yaml');
 
 // Оголошення програми для отримання параметрів з командного рядка
 const program = new Command();
@@ -14,12 +18,19 @@ program
   .requiredOption('-c, --cache <path>', 'шлях до директорії для кешу');
 
 program.parse(process.argv);
- 
+
 const options = program.opts();
 
 // Налаштування сервера
 const app = express();
 const { host, port, cache } = options;
+
+const file = fsSync.readFileSync('openapi.yaml', 'utf8');
+const swaggerDocument = YAML.parse(file);
+
+// Now you can use the app object
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(bodyParser.json());
 const upload = multer();
 
@@ -49,20 +60,18 @@ app.get('/notes/:noteName', (req, res) => {
   res.send(noteContent);
 });
 
-
 app.put('/notes/:note_name', express.text(), (req, res) => {
   const noteName = req.params.note_name;
-  const note = req.body; 
+  const note = req.body;
 
   if (!readNote(noteName)) {
     return res.status(404).send('Note not found');
   }
-  writeNote(noteName, note); 
+  writeNote(noteName, note);
   res.status(200).send('Note updated');
 });
-
 // Роут для видалення нотатки
-app.delete('/notes/:noteName', (req, res) => {0
+app.delete('/notes/:noteName', (req, res) => {
   const { noteName } = req.params;
   if (!readNote(noteName)) return res.status(404).send('Not found');
   deleteNote(noteName);
